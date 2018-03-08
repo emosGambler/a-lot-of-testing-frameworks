685dc4c8bcba9abf2cc1f25c93c28966caab8dca
Jacek Góraj <jacek.goraj@gft.com>
2018-03-08 16:21:32 +0100

    now tests are nicely described in console

--- e2e/sample-test.js
+++ e2e/sample-test.js
@@ -1,13 +1,21 @@
 var helper = require('./../utils/helper');
-var casper = require('casper').create();
 
-casper.start('http://casperjs.org/', function() {
-    this.echo(this.getTitle());
+var NUMBER_OF_TESTS = 2;
+
+casper.test.begin('Testing CasperJS page', NUMBER_OF_TESTS, function suite(test) {
+    casper.start('http://casperjs.org/', function() {
+        var randomName = 'screenshots/screenshot'.concat(helper.generateTimestamp()).concat('.png');
+        helper.captureScreenshot(casper, randomName);
+        test.assertTitle('CasperJS, a navigation scripting and testing utility for PhantomJS and SlimerJS', 'CasperJS title is wrong.');
+    });
+    
+    casper.thenOpen('http://phantomjs.org', function() {
+        var randomName = 'screenshots/screenshot'.concat(helper.generateTimestamp()).concat('.png');
+        helper.captureScreenshot(casper, randomName);
+        test.assertTitle('PhantomJS | PhantomJS', 'PhantomJS title is wrong.');
+    });
+    
+    casper.run(function() {
+        test.done();
+    });
 });
-
-casper.thenOpen('http://phantomjs.org', function() {
-    this.echo(this.getTitle());
-    helper.captureScreenshot(casper, 'screenshots/navigation.png');
-});
-
-casper.run();
